5cd76f15ae1a22ca289be067b38e078d1f5d20d1
lawf16-admin <lazywolfe16@gmail.com>
2024-08-27 16:39:02 +0900

    Revert "테스트용 커밋입니다."
    This reverts commit 6d38da818c1108185390f097365312c823961cd4.

--- src/main/java/net/datasa/web5/repository/MemberRepository.java
+++
@@ -1,9 +0,0 @@
-package net.datasa.web5.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import net.datasa.web5.domain.entity.MemberEntity;
-
-public interface MemberRepository extends JpaRepository<MemberEntity, String> {
-
-}
